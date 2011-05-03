b8704e6825e487652395d3f7f104858a4f397b0d
Millisami-Sprout <sachin@sproutifycom>
2011-05-03 18:20:06 +0545

    class Line now has dual interface, one with block variable and other without block variable

--- lib/cloud_factory/line.rb
+++ lib/cloud_factory/line.rb
@@ -1,12 +1,53 @@
 module CloudFactory
   class Line
     
-    attr_accessor :name, :input_headers
+    attr_accessor :name, :input_headers, :stations
     
-    def initialize(name, &block)
-      self.name = name
-      
+    def initialize(name)
+      @name = name
+      @input_headers = []
+      @stations = []
     end
     
+    # line = Line.new("line name")
+    # line.input_headers(InputHeader.new)
+    # line.input_headers
+    def input_headers input_header = nil
+      if input_header
+        @input_headers << input_header
+      else
+        @input_headers
+      end
+    end
+
+    # line = Line.new("line name")
+    # line.input_headers(InputHeader.new)
+    # line.input_headers
+    def stations station = nil
+      if station
+        @stations << station
+      else
+        @stations
+      end
+    end
+    
+    # Line.create("Line name") do |line|
+    #   line.input_headers InputHeader.new
+    # end
+    # 
+    # OR
+    #
+    # Line.create("Line name") do
+    #   input_headers InputHeader.new
+    # end
+    def self.create(name, &block)
+      line = Line.new(name)
+      if block.arity >= 1
+        block.call(line)
+      else
+        line.instance_eval &block
+      end
+      line
+    end
   end
 end
--- spec/cloud_factory_spec.rb
+++ spec/cloud_factory_spec.rb
@@ -1,40 +1,45 @@
 require 'spec_helper'
 
 describe CloudFactory::Line do
+  let(:input_header) { CloudFactory::InputHeader.new({:label => "image_url",:field_type => "text_data",:value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"}) }
+  
   context "create a line" do
     it "the plain ruby way" do
       line = CloudFactory::Line.new("Digitize Card")
       line.name.should eq("Digitize Card")
     end
     
-    it "using block with accessor flavor" do
-      input_header = CloudFactory::InputHeader.new({:label => "image_url",:field_type => "text_data",:value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"})
-      line = CloudFactory::Line.new("Digitize Card") do |l|
+    it "using block with variable" do
+      line = CloudFactory::Line.create("Digitize Card") do |l|
         l.input_headers << input_header
       end
       line.name.should eq("Digitize Card")
-      puts "IN #{line.input_headers}"
       line.input_headers.size.should eq(1)
       line.input_headers.first.should == input_header
     end
 
-    it "using block with methods flavor" do
-      line = CloudFactory::Line.new("Digitize Addresses") do 
-        @worker = CloudFactory::HumanWorker.new(2, 0.2)
+    it "using block without variable" do
+      line = CloudFactory::Line.create("Digitize Card") do
+        input_headers << input_header
       end
-      line.name.should eq("Digitize Addresses")
+      line.name.should eq("Digitize Card")
+      line.input_headers.size.should eq(1)
+      line.input_headers.first.should == input_header
     end
     
     context "with 1 station" do
-      xit "create with a new station" do
-        line_1 = CloudFactory::Line.new do |l|
-          l.name = "Digitize Addresses"
-          l.station = Station.new("Station 1 Name") do |station|
-            station.workers = worker
-            station.instruction = instruction
-          end
+      it "create with a new station" do
+        line_1 = CloudFactory::Line.create("Digitize Card") do |l|
+          l.stations << CloudFactory::Station.new("Station 1 Name")
+        
+          # l.station = Station.new("Station 1 Name") do |station|
+          #   station.workers = worker
+          #   station.instruction = instruction
+          # end
         end
-        line_1.name.should eq("Digitize Addresses")
+        line_1.name.should eq("Digitize Card")
+        line_1.stations.size.should eq(1)
+        line_1.stations.first.name.should eq("Station 1 Name")
       end
     end
   end
