70969a4fd65d880a0aa87f306faf98b8fabae4f3
Manish Das <manish.das@sprout-technology.com>
2011-05-04 11:32:46 +0545

    CustomInstruction created & also implemented within station block

--- spec/cloud_factory_spec.rb
+++ spec/cloud_factory_spec.rb
@@ -19,6 +19,7 @@ describe CloudFactory::Line do
     end
 
     it "using block without variable" do
+      input_header = CloudFactory::InputHeader.new({:label => "image_url",:field_type => "text_data",:value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"})
       line = CloudFactory::Line.create("Digitize Card") do
         input_headers << input_header
       end
@@ -29,18 +30,24 @@ describe CloudFactory::Line do
     
     context "with 1 station" do
       it "create with a new station" do
-        line_1 = CloudFactory::Line.create("Digitize Card") do |l|
-          l.stations << CloudFactory::Station.new("Station 1 Name")
+        worker = CloudFactory::HumanWorker.new(2, 0.2)
+        form_fields = []
+        form_fields << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
+        form_fields << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
+        form_fields << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
         
-          # l.station = Station.Create("Station 1 Name") do |station|
-          #   station.workers = worker
-          #   station.instruction = instruction
-          # end
+        line_1 = CloudFactory::Line.create("Digitize Card") do |l|
+          l.stations = CloudFactory::Station.create("Station 1 Name") do |station|
+            station.worker = worker
+            station.instruction = CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image", :description => "Describe") do |i|
+              i.form_fields = form_fields
+            end
+          end
         end
         line_1.name.should eq("Digitize Card")
-        line_1.stations.size.should eq(1)
         line_1.stations.first.name.should eq("Station 1 Name")
       end
+      
     end
   end
   
---
+++ spec/custom_instruction_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe CloudFactory::CustomInstruction do
+  context "create a stsandard_instruction" do
+    it "the plain ruby way" do
+      attrs = {:title => "Enter text from a business card image",
+        :description => "Describe"}
+      html =   '<div id="form-content">
+                  <div id="instructions">
+                    <ul>
+                      <li>Look at the business card properly and fill in asked data.</li>
+                      <li>Make sure you enter everything found on business card.</li>
+                      <li>Work may be rejected if it is incomplete or mistakes are found.</li>
+                    </ul>
+                  </div>
+                  <div id="image-field-wrapper">
+                    <div id = "image-panel" >
+                      <img class="card-mage" src="{{image_url}}">
+                    </div>
+                    <div id = "field-panel">
+                      Name<br />
+                      <input class="input-field first_name" type="text" name="result[first_name]" />
+                      <input class="input-field middle_name" type="text" name="result[middle_name]" />
+                      <input class="input-field last_name" type="text" name="result[last_name]" /><br />
+
+                      <br />Contact<br />
+                      <input class="input-field email" type="text" name="result[email]" placeholder="Email"/>
+                      <input class="input-field phone" type="text" name="result[phone]" placeholder="Phone"/>
+                      <input class="input-field mobile" type="text" name="result[mobile]" placeholder="Mobile"/><br />
+
+                    </div>
+                  </div>
+                </div>'
+                
+        css = 'body {background:#fbfbfb;}
+                #instructions{
+                  text-align:center;
+                }
+
+                #image-field-wrapper{
+                  float-left;
+                  min-width:1050px;
+                  overflow:hidden;
+                }
+
+                #field-panel{
+                  float:left;
+                  padding: 10px 10px 0 10px;
+                  min-width:512px;
+                  overflow:hidden;
+                }
+
+                .input-field{
+                  width:150px;
+                  margin:4px;
+                }'
+                
+        javascript = '<script src="http://code.jquery.com/jquery-latest.js"></script>
+                      <script type="text/javascript" src="http://www.bizcardarmy.com/javascripts/jquery.autocomplete-min.js"></script>
+                      <script type="text/javascript">
+                        $(document).ready(function(){
+                          autocomplete_fields = ["first_name", "middle_name", "last_name", "company", "job_title", "city", "state", "zip"];
+
+                          $.each(autocomplete_fields, function(index, value){
+                            var inputField = "input." + value;
+                            $(inputField).autocomplete({
+                              serviceUrl: "http://www.bizcardarmy.com/cards/return_data_for_autocompletion.json",
+                              maxHeight: 400,
+                              width: 300,
+                              zIndex: 9999,
+                              params: { field: value }
+                            });
+                          });
+                        });
+                      </script>'
+
+        instruction = CloudFactory::CustomInstruction.new(attrs)
+
+        instruction_1 = CloudFactory::CustomInstruction.create(attrs) do |i|
+          i.html = html 
+          i.css = css
+          i.javascript = javascript
+        end
+
+        instruction.title.should eq("Enter text from a business card image")
+        instruction.description.should eq("Describe")
+        instruction_1.title.should eq("Enter text from a business card image")
+        instruction_1.description.should eq("Describe")
+      end
+    end
+  end
