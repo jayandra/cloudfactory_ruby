303e38bcf71764ce13da5540c79d9c7a792739da
Manish Das <manish.das@sprout-technology.com>
2011-05-04 09:57:11 +0545

    creating worker for station both using block variable & without block variable

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -6,5 +6,27 @@ describe CloudFactory::Station do
       station = CloudFactory::Station.new("Station One")
       station.name.should eq("Station One")
     end
+
+    it "using the block variable" do
+      worker = CloudFactory::HumanWorker.new(2, 0.2)
+      station = CloudFactory::Station.create("Station 1 Name") do |s|
+        s.worker = worker
+      end
+      station.name.should eq("Station 1 Name")
+      station.worker.should == worker
+      station.worker.number.should == 2
+      station.worker.reward.should == 0.2
+    end
+    
+    it "using without the block variable" do
+      human_worker = CloudFactory::HumanWorker.new(2, 0.2)
+      station_1 = CloudFactory::Station.create("Station 1 Name") do 
+        worker human_worker
+      end
+      station_1.name.should eq("Station 1 Name")
+      station_1.worker.should == human_worker
+      station_1.worker.number.should == 2
+      station_1.worker.reward.should == 0.2
+    end
   end
 end
