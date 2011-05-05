5d3f06072de05f22d5d71367485e7ce637fd70f2
manish <manish@training1s-iMac-3.local>
2011-05-05 14:12:09 +0545

    Configuration of api key added

--- cloud_factory.gemspec
+++ cloud_factory.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "aruba"
   s.add_development_dependency "cucumber"
   s.add_development_dependency "thor"
+  s.add_development_dependency "sdoc"
 end
--- lib/cloud_factory.rb
+++ lib/cloud_factory.rb
@@ -7,6 +7,18 @@ require 'cloud_factory/station'
 require 'cloud_factory/standard_instruction'
 require 'cloud_factory/form_field'
 require 'cloud_factory/custom_instruction'
+require 'cloud_factory/run'
 
-module CloudFactory
+module CloudFactory  
+  
+  class << self
+    attr_accessor :api_key, :remote_uri
+    
+    def configure
+      yield self
+    end
+  end
+  
+  # Configuring the defaults
+  CloudFactory.remote_uri = "http://cloudfactory.com/api/v1/"
 end
