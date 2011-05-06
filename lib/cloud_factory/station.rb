f29057d4467df4c99321cafeea3ea43f5fbde8f5
manishdas <manish.das@sprout-technology.com>
2011-05-06 12:28:44 +0545

    rdoc modified

--- lib/cloud_factory/station.rb
+++ lib/cloud_factory/station.rb
@@ -1,46 +1,51 @@
 module CloudFactory
   class Station
+    # Staion name
+    attr_accessor :name
     
-    attr_accessor :name, :worker, :instruction
+    # for creating worker instance 
+    attr_accessor :worker_instance
+    
+    # for creating instruction instance
+    attr_accessor :instruction_instance
     
     # =Station class for CloudFactory api entities.
-    # ==Initializes a new Station
-    # ---
-    # * ==Usage Example:
+    # ==Initializes a new station
+    # ==Usage Example
     #     station = Station.new("station name")
     def initialize(name)
       @name = name
     end
     
     # =Station class for CloudFactory api entities.
-    # ==Initializes a new Station within block using variable
+    # ==Initializes a new station within block 
     # ---
     # 
-    # * ==Usage Example:
-    #   * ===Creating Station using block variable
-    #       worker = HumanWorker.new(2, 0.2)
-    #       form_field_value = []
-    #       form_field_value << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
-    #       form_field_value << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
-    #       form_field_value << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
-    #    
-    #       station = Station.new("station name") do |s|
-    #         s.worker = worker
+    # ==Usage Example
+    # ===Creating station using block variable
+    #     worker = HumanWorker.new(2, 0.2)
+    #     form_field_value = []
+    #     form_field_value << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
+    #     form_field_value << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
+    #     form_field_value << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
+    #       
+    #     station = Station.new("station name") do |s|
+    #       s.worker = worker
+    #       
+    #       s.instruction = CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image",
+    #                             :description => "Describe") do |i|
+    #     
+    #         i.form_fields = form_field_value
+    #       end
+    # ===OR creating without variable within block
+    #     station = Station.new("station name") do
+    #       worker worker
     #
-    #         s.instruction = CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image",
-    #                               :description => "Describe") do |i|
+    #       instruction CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image", 
+    #                             :description => "Describe") do |i|
     #
-    #           i.form_fields = form_field_value
-    #         end
-    #   * ===OR creating without variable within block
-    #       station = Station.new("station name") do
-    #         worker worker
-    #
-    #         instruction CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image", 
-    #                               :description => "Describe") do |i|
-    #
-    #          i.form_fields = form_field_value
-    #         end
+    #         i.form_fields = form_field_value
+    #       end
     def self.create(name, &block)
       station = Station.new(name)
       if block.arity >= 1
@@ -51,33 +56,37 @@ module CloudFactory
       station
     end
     
-    # ==Usage of Station.worker method
-    # ---
-    # * ==Usage Example:
+    # * Creates new instruction for station object
+    # ==Usage of worker method for "station" object
     #     worker = HumanWorker.new(2, 0.2)
-    #
-    #     Station.worker = worker  
-    def worker worker = nil
-      if worker
-        @worker = worker
+    #     
+    #     station.worker = worker
+    def worker worker_instance = nil
+      if worker_instance
+        @worker_instance = worker_instance
       else
-        @worker
+        @worker_instance
       end
     end
     
-    # ==Usage of Station.instruction method
-    # ---
-    # * ==Usage Example:
-    #     instruction = HumanWorker.new(2, 0.2)
+    # * Creates new instruction for station object
+    # ==Usage of Instruction method for "station" object
     #
-    #     Station.worker = CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image", :description => "Describe") do |i|
-    #       i.form_fields = form_field_value
-    #     end
-    def instruction instruction = nil
-      if instruction
-        @instruction = instruction
+    #       form_field_value = []
+    #       form_field_value << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
+    #       form_field_value << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
+    #       form_field_value << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")    
+    #
+    #       station.instruction = CloudFactory::StandardInstruction.create(:title => "Enter text from a business card image",
+    #                             :description => "Describe") do |i|
+    #
+    #         i.form_fields = form_field_value
+    #       end
+    def instruction instruction_instance = nil
+      if instruction_instance
+        @instruction_instance = instruction_instance
       else
-        @instruction
+        @instruction_instance
       end
     end
     
