151e0d81955694872efca1b07237e9fc19f9a981
manishdas <manish.das@sprout-technology.com>
2011-05-09 16:59:16 +0545

    Rdo added for Run Class

--- lib/cloud_factory/standard_instruction.rb
+++ lib/cloud_factory/standard_instruction.rb
@@ -1,43 +1,45 @@
 module CloudFactory
   class StandardInstruction
     
-    attr_accessor :title, :description, :form_fields
+    # title of the standard_instruction
+    attr_accessor :title
     
-    # =StandardInstruction class for CloudFactory api entities.
+    # description about the instruction
+    attr_accessor :description
+    
+    # form_fields for the StandardInstruction
+    attr_accessor :form_fields
+    
     # ==Initializes a new StandardInstruction
-    # ---
-    # * ==Usage Example:
-    #     attrs = {:title => "Enter text from a business card image",
-    #         :description => "Describe"}
+    # ==Usage of standard_instruction.new
+    #   attrs = {:title => "Enter text from a business card image",
+    #       :description => "Describe"}
     #
-    #     instruction = StandardInstruction.new(attrs)
+    #   instruction = StandardInstruction.new(attrs)
     def initialize(options={})
       @title       = options[:title]
       @description = options[:description]
     end
     
-    # =StandardInstruction class for CloudFactory api entities.
     # ==Initializes a new StandardInstruction within block using Variable
-    # ---
-    # * ==Usage Example:
-    #   * ===Creating StandardInstruction using block variable
-    #       attrs = {:title => "Enter text from a business card image",
-    #           :description => "Describe"}
+    # ==Usage of "standard_instruction.create(hash) do |block|"
+    # ===Creating StandardInstruction using block variable
+    #   attrs = {:title => "Enter text from a business card image",
+    #       :description => "Describe"}
     #
-    #       form_field_values = []
-    #       form_field_values << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
-    #       form_field_values << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
-    #       form_field_values << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
+    #   form_field_values = []
+    #   form_field_values << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
+    #   form_field_values << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
+    #   form_field_values << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
     #
-    #       instruction = CloudFactory::StandardInstruction.create(attrs) do |i|
-    #         i.form_fields = form_fields_values
-    #       end
+    #   instruction = CloudFactory::StandardInstruction.create(attrs) do |i|
+    #     i.form_fields = form_fields_values
+    #   end
     #
-    #   * ===OR without block variable
-    #       instruction = CloudFactory::StandardInstruction.create(attrs) do |i|
-    #         form_fields form_fields_values
-    #       end
-    #--
+    # ===OR without block variable
+    #   instruction = CloudFactory::StandardInstruction.create(attrs) do |i|
+    #     form_fields form_fields_values
+    #   end
     def self.create(instruction, &block)
       instruction = StandardInstruction.new(instruction)
       if block.arity >= 1
@@ -48,17 +50,15 @@ module CloudFactory
       instruction
     end
     
-    # ==Usage of formfield
-    # ---
-    # * Syntax for FormField method is form_fields << form_field_values
-    # * ==Usage Example:
-    #     form_field_values = []
-    #     form_field_values << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
-    #     form_field_values << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
-    #     form_field_values << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
+    # ==appending different form_fields 
+    # ===Syntax for FormField method is form_fields << form_field_values
+    # ==Usage of form_fields method
+    #   form_field_values = []
+    #   form_field_values << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
+    #   form_field_values << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
+    #   form_field_values << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
     #
-    #     form_fields << form_field_values
-    #
+    #   form_fields << form_field_values
     def form_fields form_fields = nil
       if form_fields
         @form_fields = form_fields
