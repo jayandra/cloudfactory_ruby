f4c66402261e9dd7bcc8e92dd0f272608380d69c
manishdas <manish.das@sprout-technology.com>
2011-05-12 13:40:04 +0545

    category added

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe CloudFactory::Category do
   it "should get all the categories" do
     categories = CloudFactory::Category.all
-    
+    categories[0]._id.should eq("4db9319f703cf14825000001")
+    categories[0].name.should eq("Digitization")
+    categories[1]._id.should eq("4db9319f703cf14825000002")
+    categories[1].name.should eq("Data Processing")
   end
 end
