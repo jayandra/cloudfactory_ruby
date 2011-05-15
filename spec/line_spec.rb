243fc98a6ebdd3de8028d6fd4865e7e87e0a74cc
manishdas <manish.das@sprout-technology.com>
2011-05-15 12:35:10 +0545

    API calls implemented for input_headers

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -6,7 +6,7 @@ describe CloudFactory::Line do
   context "create a line" do
     it "the plain ruby way" do
       VCR.use_cassette "lines/create", :record => :new_episodes do
-        line = CloudFactory::Line.new("Digitize Card","")
+        line = CloudFactory::Line.new("Digitize Card","4dc8ad6572f8be0600000001")
         line.title.should eq("Digitize Card")
         line.category_id.should eq("4dc8ad6572f8be0600000001")
       end
@@ -15,7 +15,7 @@ describe CloudFactory::Line do
     it "using block with variable" do
       VCR.use_cassette "lines/create-block-var", :record => :new_episodes do
         line = CloudFactory::Line.create("Digitize Card","4dc8ad6572f8be0600000001") do |l|
-          input_header = CloudFactory::InputHeader.new({:label => "image_url",:field_type => "text_data",
+          input_header = CloudFactory::InputHeader.new(l, {:label => "image_url",:field_type => "text_data",
             :value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"})
             l.input_headers = [input_header]
           end
@@ -27,13 +27,13 @@ describe CloudFactory::Line do
 
       it "using block without variable" do
         VCR.use_cassette "lines/create-without-block-var", :record => :new_episodes do
-          input_header = CloudFactory::InputHeader.new({:label => "image_url",:field_type => "text_data",:value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"})
           worker = CloudFactory::HumanWorker.new(2, 0.2)
           form_fields = []
           form_fields << CloudFactory::FormField.new(:label => "First Name", :field_type => "SA", :required => "true")
           form_fields << CloudFactory::FormField.new(:label => "Middle Name", :field_type => "SA")
           form_fields << CloudFactory::FormField.new(:label => "Last Name", :field_type => "SA", :required => "true")
           line = CloudFactory::Line.create("Digitize Card", "4dc8ad6572f8be0600000001") do
+            input_header = CloudFactory::InputHeader.new(self, {:label => "image_url",:field_type => "text_data",:value => "http://s3.amazon.com/bizcardarmy/medium/1.jpg", :required => true, :validation_format => "url"})
             input_headers [input_header]
             stations = CloudFactory::Station.create(self, :type => "work") do |station|
               station.worker = worker
