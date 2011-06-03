34ee4062e8280abcede4ac79a239b83f20299e8f
manishdas <manish.das@sprout-technology.com>
2011-06-03 11:57:14 +0545

    Plain ruby implemented and in order to use both plain ruby way and Block DSL way few modifications are made in coes

--- spec/exceptions_spec.rb
+++ spec/exceptions_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Exceptions" do
   context "Line" do
-    it "without name and department should raise ArgumentError" do
+    xit "without name and department should raise ArgumentError" do
       WebMock.allow_net_connect!
       # VCR.use_cassette "lines/plain-ruby/create-station", :record => :new_episodes do
       line = CloudFactory::Line.new("Digitize Card", "Digitization")
