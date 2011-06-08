9eb92e0205e096a627657d7b2e878dbc2d7264f6
Manish <manish@Manishs-iMac.local>
2011-06-08 17:09:10 +0545

    Tests passed after pulling new changes from cloudfactory repo, run created without uploading file for input data

--- example/human_worker_app/human_worker_app.rb
+++ example/human_worker_app/human_worker_app.rb
@@ -15,22 +15,19 @@ class HumanWorkerApp < Sinatra::Base
   end
 
   get '/run' do
-    # CloudFactory.api_url = "sprout.lvh.me:3000/api/"
     response = RestClient.get 'http://sprout.lvh.me:3000/api/v1/lines.json', {:accept => :json}
-    # @categories = CloudFactory::Category.all
-# Tournament  
     line = CloudFactory::Line.create("Nepali Politicians","Survey") do |l|
       CloudFactory::Station.create({:line => l, :type => "tournament"}) do |s|
         CloudFactory::InputHeader.new({:station => s, :label => "Politician Name",:field_type => "text_data",:value => "Madhav Kumar Nepal", :required => true, :validation_format => "general"})
         CloudFactory::HumanWorker.new({:station => s, :number => 1, :reward => 20})
         CloudFactory::Form.create({:station => s, :title => "Guess the Amount", :description => "Guess the amount they have earned through bribe in Rupees"}) do |f|
-          CloudFactory::FormField.new({:instruction => f, :label => "Amount", :field_type => "SA", :required => "true"})          
+          CloudFactory::FormField.new({:instruction => f, :label => "Amount", :field_type => "SA", :required => "true"})
         end
       end
     end
 
     run = CloudFactory::Run.create(line, "2011 Ghotala", File.expand_path("human_worker_input.csv", __FILE__))
-    
+
     haml :run
   end
 
--- lib/cloud_factory/run.rb
+++ lib/cloud_factory/run.rb
@@ -6,16 +6,16 @@ module CloudFactory
     attr_accessor :title
 
     # file attributes to upload
-    attr_accessor :file
-    
+    attr_accessor :file, :data
+
     # line attribute with which run is associated
     attr_accessor :line
 
     attr_accessor :id
-    
+
     # ==Initializes a new Run
     # ==Usage Example:
-    #     
+    #
     #   line = CloudFactory::Line.create("Digitize Card","Digitization") do |l|
     #     CloudFactory::InputHeader.new({:label => "Company", :field_type => "text_data", :value => "Google", :required => true, :validation_format => "general"})
     #     CloudFactory::InputHeader.new({:label => "Website", :field_type => "text_data", :value => "www.google.com", :required => true, :validation_format => "url"})
@@ -24,24 +24,32 @@ module CloudFactory
     #       CloudFactory::StandardInstruction.create(:station => s, :title => "Enter text from a business card image", :description => "Describe"}) do |i|
     #         CloudFactory::FormField.new({:instruction => i, :label => "First Name", :field_type => "SA", :required => "true"})
     #         CloudFactory::FormField.new({:instruction => i, :label => "Middle Name", :field_type => "SA"})
-    #         CloudFactory::FormField.new({:instruction => i, :label => "Last Name", :field_type => "SA", :required => "true"})            
+    #         CloudFactory::FormField.new({:instruction => i, :label => "Last Name", :field_type => "SA", :required => "true"})
     #       end
     #     end
     #   end
-    # 
+    #
     #   run = CloudFactory::Run.new(line, "run name", File.expand_path("../../fixtures/input_data/test.csv", __FILE__))
-    def initialize(line, title, file)
+    def initialize(line, title, input)
       @line = line
       @title = title
-      @file = file
+      if File.exist?(input)
+        @file = input
+        @@param_data = File.new(input, 'rb')
+        @@param_for_input = :file
+      else
+        @data = input
+        @@param_data = input
+        @@param_for_input = :data
+      end
       @input_data =[]
-      resp = self.class.post("/lines/#{@line.id}/runs.json", {:run => {:title => @title}, :file => File.new(@file, 'rb')})
+      resp = self.class.post("/lines/#{@line.id}/runs.json", {:run => {:title => @title}, @@param_for_input => @@param_data })
       @id = resp.id
     end
-    
+
     # ==Creates a new Run
     # ==Usage Example:
-    #     
+    #
     #   line = CloudFactory::Line.create("Digitize Card","Digitization") do |l|
     #     CloudFactory::InputHeader.new({:line => l, :label => "Company", :field_type => "text_data", :value => "Google", :required => true, :validation_format => "general"})
     #     CloudFactory::InputHeader.new({:line => l, :label => "Website", :field_type => "text_data", :value => "www.google.com", :required => true, :validation_format => "url"})
@@ -50,7 +58,7 @@ module CloudFactory
     #       CloudFactory::StandardInstruction.create(:station => s, :title => "Enter text from a business card image", :description => "Describe"}) do |i|
     #         CloudFactory::FormField.new({:instruction => i, :label => "First Name", :field_type => "SA", :required => "true"})
     #         CloudFactory::FormField.new({:instruction => i, :label => "Middle Name", :field_type => "SA"})
-    #         CloudFactory::FormField.new({:instruction => i, :label => "Last Name", :field_type => "SA", :required => "true"})            
+    #         CloudFactory::FormField.new({:instruction => i, :label => "Last Name", :field_type => "SA", :required => "true"})
     #       end
     #     end
     #   end
@@ -59,7 +67,7 @@ module CloudFactory
     def self.create(line, title, file)
       Run.new(line, title, file)
     end
-    
+
     def get # :nodoc:
       self.class.get("/lines/#{@line.id}/runs/#{@id}.json")
     end
