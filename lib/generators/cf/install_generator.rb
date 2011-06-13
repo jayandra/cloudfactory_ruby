08a21c576e63f4bcd2c4adf435c01138c564371b
MillisamiSprout <sachin@sprout-technology.com>
2011-06-13 17:18:19 +0545

    simple initializer generator for Rails app

---
+++ lib/generators/cf/install_generator.rb
@@ -0,0 +1,18 @@
+module CF
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Creates a CF initializer"
+      def generate_initializer
+        say_status("Generating", "Cloud Factory initializer", :green)
+        copy_file "cloud_factory.rb", "config/initializers/cloud_factory.rb"
+      end
+      
+      def show_readme
+        readme 'USAGE' if behavior == :invoke
+      end
+      
+    end
+  end  
+end
