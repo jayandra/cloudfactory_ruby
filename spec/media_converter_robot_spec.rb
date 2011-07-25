c9429a4516b9e36237400b3d149eea7518364134
manishdas <manish.das@sprout-technology.com>
2011-07-25 12:40:10 +0545

    Media Converter Robot bugs fixed

--- spec/media_converter_robot_spec.rb
+++ spec/media_converter_robot_spec.rb
@@ -5,8 +5,8 @@ module CF
     context "create a media converter robot worker" do
       it "should create media_converter_robot worker for first station in a plain ruby way" do
         # WebMock.allow_net_connect!
-        VCR.use_cassette "media_converter_robot/block/create-worker-in-first-station", :record => :new_episodes do
-          line = CF::Line.new("media-line","Digitization")
+        VCR.use_cassette "media_converter_robot/plain/create-worker-in-first-station", :record => :new_episodes do
+          line = CF::Line.new("media-line1","Digitization")
 
           input_format = CF::InputFormat.new({:name => "url", :required => true, :valid_type => "url"})
           line.input_formats input_format
@@ -31,22 +31,22 @@ module CF
           form_fields = CF::FormField.new({:label => "Description", :field_type => "SA", :required => "true"})
           line.stations.first.form.form_fields form_fields
 
-          run = CF::Run.create(line, "media-converter-robot", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
+          run = CF::Run.create(line, "media-converter-robot1", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
 
           @final_output = run.final_output
           line.stations.first.worker.number.should eq(1)
-          @final_output.first.final_outputs.first['audio_quality'].should eql("320")
-          @final_output.first.final_outputs.first['video_quality'].should eql("3")
-          @final_output.first.final_outputs.first['to'].should eql("mpg")
-          converted_url = @final_output.first.final_outputs.first['url']
+          @final_output.first.final_output.first.audio_quality.should eql("320")
+          @final_output.first.final_output.first.video_quality.should eql("3")
+          @final_output.first.final_output.first.to.should eql("mpg")
+          converted_url = @final_output.first.final_output.first.converted_file_from_url.first
           File.exist?("/Users/manish/apps/cloudfactory/public#{converted_url}").should eql(true)
         end
       end
 
       it "should create media_converter_robot worker for first station in a plain ruby way without passing station parameter within the syntax" do
         # WebMock.allow_net_connect!
-        VCR.use_cassette "media_converter_robot/block/create-worker-in-plain-ruby-way", :record => :new_episodes do
-          line = CF::Line.new("media-line-2","Digitization")
+        VCR.use_cassette "media_converter_robot/plain/create-worker-in-plain-ruby-way", :record => :new_episodes do
+          line = CF::Line.new("media-line-21","Digitization")
 
           input_format = CF::InputFormat.new({:name => "url", :required => true, :valid_type => "url"})
           line.input_formats input_format
@@ -72,14 +72,14 @@ module CF
           form_fields = CF::FormField.new({:label => "Description", :field_type => "SA", :required => "true"})
           line.stations.first.form.form_fields form_fields
 
-          run = CF::Run.create(line, "media-converter-robot-2", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
+          run = CF::Run.create(line, "media-converter-robot-21", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
 
           @final_output = run.final_output
           line.stations.first.worker.number.should eq(1)
-          @final_output.first.final_outputs.first['audio_quality'].should eql("320")
-          @final_output.first.final_outputs.first['video_quality'].should eql("3")
-          @final_output.first.final_outputs.first['to'].should eql("mpg")
-          converted_url = @final_output.first.final_outputs.first['url']
+          @final_output.first.final_output.first.audio_quality.should eql("320")
+          @final_output.first.final_output.first.video_quality.should eql("3")
+          @final_output.first.final_output.first.to.should eql("mpg")
+          converted_url = @final_output.first.final_output.first.converted_file_from_url.first
           File.exist?("/Users/manish/apps/cloudfactory/public#{converted_url}").should eql(true)
         end
       end
@@ -87,7 +87,7 @@ module CF
       it "should create media_converter_robot worker for multiple station in a plain ruby way" do
         # WebMock.allow_net_connect!
         VCR.use_cassette "media_converter_robot/block/create-worker-multiple-station", :record => :new_episodes do
-          line = CF::Line.create("media-line-4","Digitization") do |l|
+          line = CF::Line.create("media-line-41","Digitization") do |l|
             CF::InputFormat.new({:line => l, :name => "Company", :required => true, :valid_type => "general"})
             CF::InputFormat.new({:line => l, :name => "Website", :required => true, :valid_type => "url"})
             CF::InputFormat.new({:line => l, :name => "url", :required => true, :valid_type => "url"})
@@ -117,21 +117,21 @@ module CF
           form_fields = CF::FormField.new({:label => "Description", :field_type => "SA", :required => "true"})
           line.stations.last.form.form_fields form_fields
 
-          run = CF::Run.create(line, "media-converter-robot-4", [{"Company" => "Apple", "Website" => "www.apple.com", "url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
+          run = CF::Run.create(line, "media-converter-robot-41", [{"Company" => "Apple", "Website" => "www.apple.com", "url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
 
           # debugger
           @final_output = run.final_output
           line.stations.last.worker.number.should eq(1)
-          @final_output.first.final_outputs.first['audio_quality'].should eql("320")
-          @final_output.first.final_outputs.first['video_quality'].should eql("3")
-          @final_output.first.final_outputs.first['to'].should eql("mpg")
+          @final_output.first.final_output.first.audio_quality.should eql("320")
+          @final_output.first.final_output.first.video_quality.should eql("3")
+          @final_output.first.final_output.first.to.should eql("mpg")
         end
       end
 
       it "should create media_converter_robot in block DSL way" do
         # WebMock.allow_net_connect!
         VCR.use_cassette "media_converter_robot/block/create-worker-block-dsl-way", :record => :new_episodes do
-          line = CF::Line.create("media-line-5","Digitization") do |l|
+          line = CF::Line.create("media-line-52","Digitization") do |l|
             CF::InputFormat.new({:line => l, :name => "url", :required => true, :valid_type => "url"})
             CF::InputFormat.new({:line => l, :name => "to", :required => false, :valid_type => "general"})
             CF::InputFormat.new({:line => l, :name => "audio_quality", :required => false, :valid_type => "general"})
@@ -144,14 +144,14 @@ module CF
             end
           end
 
-          run = CF::Run.create(line, "media-converter-robot-5", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
+          run = CF::Run.create(line, "media-converter-robot-52", [{"url"=> "www.s3.amazon.com/converted/20110518165230/ten.mpg", "to" => "mpg", "audio_quality" => "320", "video_quality" => "3",  "meta_data_text"=>"media"}])
 
           @final_output = run.final_output
           line.stations.first.worker.number.should eq(1)
-          @final_output.first.final_outputs.first['audio_quality'].should eql("320")
-          @final_output.first.final_outputs.first['video_quality'].should eql("3")
-          @final_output.first.final_outputs.first['to'].should eql("mpg")
-          converted_url = @final_output.first.final_outputs.first['url']
+          @final_output.first.final_output.first.audio_quality.should eql("320")
+          @final_output.first.final_output.first.video_quality.should eql("3")
+          @final_output.first.final_output.first.to.should eql("mpg")
+          converted_url = @final_output.first.final_output.first.converted_file_from_url.first
           File.exist?("/Users/manish/apps/cloudfactory/public#{converted_url}").should eql(true)
         end
       end
