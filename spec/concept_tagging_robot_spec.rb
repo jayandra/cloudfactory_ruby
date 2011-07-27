0e42a3743a50d66c30461d385fd7bcdebee267a0
manishdas <manish.das@sprout-technology.com>
2011-07-27 16:43:32 +0545

    Robot workers spec was modified according to new change in Roobot Workers

--- spec/concept_tagging_robot_spec.rb
+++ spec/concept_tagging_robot_spec.rb
@@ -13,12 +13,12 @@ module CF
               CF::ConceptTaggingRobot.create({:station => s, :url => ["{url}"]})
             end
           end
-          run = CF::Run.create(line, "concept_tagging_robot_run_1", [{"url"=> "www.pornhub.com"}])
+          run = CF::Run.create(line, "concept_tagging_robot_run_1", [{"url"=>"www.mosexindex.com"}])
           output = run.final_output
-          output.first.final_output.first.concept_tagging_of_url.should eql(["Pornography", "Pornography addiction", "Pornographic actor", "Amateur pornography", "Internet", "Reality pornography", "Pornographic film"])
-          output.first.final_output.first.concept_tagging_relevance_of_url.should eql([96.2012, 80.8069, 71.2139, 66.0961, 63.461999999999996, 58.9256, 54.413999999999994])
+          output.first.final_output.first.concept_tagging_of_url.should eql(["Canada", "English language"])
+          output.first.final_output.first.concept_tagging_relevance_of_url.should eql([89.5153, 79.0912])
           line.stations.first.worker.class.should eql(CF::ConceptTaggingRobot)
-          line.stations.first.worker.reward.should eql(500)
+          line.stations.first.worker.reward.should eql(1)
           line.stations.first.worker.number.should eql(1)
           line.stations.first.worker.url.should eql(["{url}"])
         end
@@ -37,12 +37,12 @@ module CF
           worker = CF::ConceptTaggingRobot.create({:url => ["{url}"]})
           line.stations.first.worker = worker
 
-          run = CF::Run.create(line, "concept_tagging_robot_run_2", [{"url"=> "www.pornhub.com"}])
+          run = CF::Run.create(line, "concept_tagging_robot_run_1", [{"url"=>"www.mosexindex.com"}])
           output = run.final_output
-          output.first.final_output.first.concept_tagging_of_url.should eql(["Pornography", "Pornography addiction", "Pornographic actor", "Amateur pornography", "Internet", "Reality pornography", "Pornographic film"])
-          output.first.final_output.first.concept_tagging_relevance_of_url.should eql([96.2012, 80.8069, 71.2139, 66.0961, 63.461999999999996, 58.9256, 54.413999999999994])
+          output.first.final_output.first.concept_tagging_of_url.should eql(["Canada", "English language"])
+          output.first.final_output.first.concept_tagging_relevance_of_url.should eql([89.5153, 79.0912])
           line.stations.first.worker.class.should eql(CF::ConceptTaggingRobot)
-          line.stations.first.worker.reward.should eql(500)
+          line.stations.first.worker.reward.should eql(1)
           line.stations.first.worker.number.should eql(1)
           line.stations.first.worker.url.should eql(["{url}"])
         end
