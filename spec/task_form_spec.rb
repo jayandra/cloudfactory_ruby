1438e97edd1d582deaad1a1f9c1ffa02369ba138
manishdas <manish.das@sprout-technology.com>
2011-07-28 16:35:19 +0545

    tests were passed for the errors and modified accordingly

--- spec/task_form_spec.rb
+++ spec/task_form_spec.rb
@@ -24,8 +24,27 @@ describe CF::TaskForm do
         form.form_fields.first.required.should eq(true)
       end
     end
+
+    xit "with invalid data" do
+      WebMock.allow_net_connect!
+      # VCR.use_cassette "task_form/block/create", :record => :new_episodes do
+      line = CF::Line.create("Digiti-ard-2", "Digitization") do
+        CF::InputFormat.new({:line => self, :name => "image_url", :required => true, :valid_type => "url"})
+        CF::Station.create({:line => self, :type => "work"}) do |station|
+          CF::HumanWorker.new({:station => station, :number => 2, :reward => 20})
+          CF::TaskForm.new({:station => station})
+        end
+      end
+      # debugger
+      form = line.stations[0].form
+      form.instruction.should eq("Describe")
+      form.form_fields.first.label.should eq("First Name")
+      form.form_fields.first.field_type.should eq("SA")
+      form.form_fields.first.required.should eq(true)
+      # end
+    end
   end
-  
+
   context "get instruction info" do
     it "should get all the instruction information of a station" do
       # WebMock.allow_net_connect!
@@ -100,7 +119,7 @@ describe CF::TaskForm do
 
         station = line.stations[0]
         deleted_response = CF::TaskForm.delete_instruction(station)
-        
+
         deleted_response.code.should eq(200)
       end
     end
