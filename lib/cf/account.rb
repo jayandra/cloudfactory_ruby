e337edb1d73e414071ca846efbd56d8db138be8a
MillisamiSprout <sachin@sprout-technology.com>
2011-08-01 15:44:47 +0545

    explicit return in CF::Account.info

--- lib/cf/account.rb
+++ lib/cf/account.rb
@@ -11,12 +11,21 @@ module CF
         if resp.code != 200
           self.errors = resp.error.message
         end
-
+        return resp
       end
 
       def valid?
         info
         errors.nil?
+      end
+      
+      def login(email, passwd)
+        resp = post('/api_login.json', {:email => email, :password => passwd})
+        
+        if resp.code != 200
+          self.errors = resp.error.message
+        end
+        return resp
       end
     end
   end
